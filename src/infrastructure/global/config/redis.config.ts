ca5d08facfc461a399c722fb8956e1aece1a5878
4mjeo <4mj.eo@dsm.hs.kr>
2024-08-31 13:58:20 +0900

    feat: redis config 추가

---
+++ src/infrastructure/global/config/redis.config.ts
@@ -0,0 +1,27 @@
+import { CacheModule } from '@nestjs/cache-manager';
+import { RedisClientOptions } from 'redis';
+import { redisStore } from 'cache-manager-redis-yet';
+import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { RefreshTokenRepository } from '../../domain/auth/persistence/refresh-token.repository';
+
+@Module({
+    imports: [
+        CacheModule.registerAsync<RedisClientOptions>({
+            inject: [ConfigService],
+            useFactory: async (config: ConfigService) => ({
+                store: await redisStore({
+                    socket: {
+                        host: config.get<string>('REDIS_HOST'),
+                        port: config.get<number>('REDIS_PORT')
+                    },
+                    password: config.get<string>('REDIS_PASSWORD')
+                })
+            })
+        })
+    ],
+    providers: [RefreshTokenRepository],
+    exports: [RefreshTokenRepository]
+})
+export class RedisCacheModule {
+}
