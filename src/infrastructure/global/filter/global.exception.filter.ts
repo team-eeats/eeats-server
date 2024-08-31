c1f94ea9fcf00c6b1e6185a432cddd5652d0b005
4mjeo <4mj.eo@dsm.hs.kr>
2024-08-31 13:58:39 +0900

    feat: exception filter 추가

---
+++ src/infrastructure/global/filter/global.exception.filter.ts
@@ -0,0 +1,31 @@
+import { ArgumentsHost, Catch, ExceptionFilter, HttpException, HttpStatus } from '@nestjs/common';
+import { responseEncoding } from 'axios';
+import { Request, Response } from 'express';
+import { stat } from 'fs';
+
+@Catch()
+export class GlobalExceptionFilter implements ExceptionFilter {
+    catch(exception: unknown, host: ArgumentsHost): void {
+        const ctx = host.switchToHttp();
+        const request = ctx.getRequest<Request>();
+        const response = ctx.getResponse<Response>();
+
+        let status = HttpStatus.INTERNAL_SERVER_ERROR;
+        let message = 'Internal Server Error';
+        if (exception instanceof HttpException) {
+            status = exception.getStatus();
+            message = exception.message;
+        } else {
+            console.error(exception);
+        }
+
+        const resposneBody = {
+            statusCode: status,
+            message: message,
+            timestamp: new Date().toISOString(),
+            path: request.path
+        };
+
+        response.status(status).json(resposneBody);
+    }
+}
