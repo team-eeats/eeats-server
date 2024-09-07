a0fcc377c45ab47dc7b977f9b46c668aa9bbda59
4mjeo <4mj.eo@dsm.hs.kr>
2024-09-07 22:40:11 +0900

    update: parsing meal info

--- src/infrastructure/thirdparty/axios/axios.adapter.ts
+++ src/infrastructure/thirdparty/axios/axios.adapter.ts
@@ -6,19 +6,61 @@ import { ConfigService } from '@nestjs/config';
 @Injectable()
 export class AxiosAdapter implements MealPort {
     constructor(private readonly configService: ConfigService) {}
-
-    async getMealInfo(schoolCode: string, date: string): Promise<any> {
+    
+    async getMealInfo(date: string): Promise<any> {
         const NEIS_API_BASE_URL = 'https://open.neis.go.kr/hub/mealServiceDietInfo';
 
         const params = {
             ATPT_OFCDC_SC_CODE: this.configService.get<string>('NEIS_ATPT_CODE'),
-            SD_SCHUL_CODE: schoolCode,
+            SD_SCHUL_CODE: this.configService.get<string>('SCHOOL_CODE'),
             MLSV_YMD: date,
             KEY: this.configService.get<string>('NEIS_API_KEY'),
             Type: 'json'
         };
 
         const response = await axios.get(NEIS_API_BASE_URL, { params });
-        return response.data.mealServiceDietInfo[1].row;
+        const mealData = response.data.mealServiceDietInfo[1].row;
+
+        const meals = this.parseMealData(mealData);
+
+        return meals;
+    }
+
+    private parseMealData(mealData: any[]): any {
+        const parsedMeals: { [key: string]: string[] } = {
+            breakfast: [],
+            lunch: [],
+            dinner: []
+        };
+
+        mealData.forEach(meal => {
+            const mealType = this.getMealType(meal.MMEAL_SC_CODE);
+
+            parsedMeals[mealType].push(
+                meal.DDISH_NM.replace(/<br\/>/g, ', ').replace(/\n/g, ', '),
+                meal.CAL_INFO
+            );
+        });
+
+        Object.keys(parsedMeals).forEach(mealType => {
+            if (parsedMeals[mealType].length === 0) {
+                parsedMeals[mealType].push('급식이 없습니다.');
+            }
+        });
+
+        return parsedMeals;
+    }
+
+    private getMealType(mealCode: string): string {
+        switch (mealCode) {
+            case '1':
+                return 'breakfast';
+            case '2':
+                return 'lunch';
+            case '3':
+                return 'dinner';
+            default:
+                return 'unknown';
+        }
     }
 }
