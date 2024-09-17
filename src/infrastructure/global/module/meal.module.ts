a56888b4e545d8b01babf52c0ae684495b907452
4mjeo <4mj.eo@dsm.hs.kr>
2024-09-17 15:35:13 +0900

    refactor: (#12) axios module로 통합

--- src/infrastructure/global/module/meal.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { GetMealUseCase } from '../../../application/domain/meal/usecase/get-meal.usecase';
-import { MealWebAdapter } from '../../domain/meal/presentation/meal.web.adapter';
-
-@Module({
-    providers: [GetMealUseCase],
-    controllers: [MealWebAdapter]
-})
-export class MealModule {}
