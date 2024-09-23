6d6f52fc15f43e25a184c51531b0879e97c7e31c
김명진 <4mj.eo@dsm.hs.kr>
2024-09-23 23:06:58 +0900

    style: (#16) module import 간결화

--- src/infrastructure/global/module/axios.module.ts
+++ src/infrastructure/global/module/axios.module.ts
@@ -2,7 +2,7 @@ import { Global, Module } from '@nestjs/common';
 import { AxiosPort } from '../../../application/common/spi/axios.spi';
 import { AxiosAdapter } from '../../thirdparty/axios/axios.adapter';
 import { GetMealUseCase } from '../../../application/domain/meal/usecase/get-meal.usecase';
-import { MealWebAdapter } from 'src/infrastructure/domain/meal/presentation/meal.web.adapter';
+import { MealWebAdapter } from '../../domain/meal/presentation/meal.web.adapter';
 
 const MEAL_PORT = { provide: AxiosPort, useClass: AxiosAdapter };
 
