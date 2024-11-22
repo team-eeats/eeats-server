cc802e3b2815964ba5a9c78aa3f1abcb5bb7b53d
김명진 <4mj.eo@dsm.hs.kr>
2024-11-22 11:31:34 +0900

    feat: (#43) 진행 중 확인 컬럼 추가

--- src/application/domain/poll/poll.ts
+++ src/application/domain/poll/poll.ts
@@ -9,6 +9,7 @@ export class Poll {
     endDate: LocalDateTime;
     options: PollOption[];
     createdAt: LocalDateTime;
+    isActive: boolean;
 
     public update(title: string, description: string, startDate: LocalDateTime, endDate: LocalDateTime) {
         this.title = title;
